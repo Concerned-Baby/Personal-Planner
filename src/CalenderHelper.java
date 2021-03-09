6f18d74d218df65310a5bca761783e75d5c00ea6
Spencer Ye <sye348@student.fuhsd.org>
2021-03-08 19:56:53 -0800

    add constants

--- src/CalenderHelper.java
+++ src/CalenderHelper.java
@@ -2,7 +2,27 @@
 public class CalenderHelper
 {
 	
-	//TODO add all the constatnts (months and weekdays)
+	//TODO add all the constants (months and weekdays)
+	public static final int JANUARY = 1567;
+	public static final int FEBRUARY = 2567;
+	public static final int MARCH = 3567;
+	public static final int APRIL = 4567;
+	public static final int MAY = 5567;
+	public static final int JUNE = 6567;
+	public static final int JULY = 7567;
+	public static final int AUGUST = 8567;
+	public static final int SEPTEMBER = 9567;
+	public static final int OCTOBER = 10567;
+	public static final int NOVEMBER = 11567;
+	public static final int DECEMBER = 12567;
+	
+	public static final int MONDAY = 1987;
+	public static final int TUESDAY = 2987;
+	public static final int WEDNESDAY = 3987;
+	public static final int THURSDAY = 4987;
+	public static final int FRIDAY = 5987;
+	public static final int SATURDAY = 6987;
+	public static final int SUNDAY = 7987;
 	
 	
 	public static int getMonthLength(int month)
