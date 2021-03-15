97c91bc90c317e392c9074a6438b6d749747785d
Spencer Ye <sye348@student.fuhsd.org>
2021-03-15 00:11:00 -0700

    add unimplemented methods

--- src/dataformats/WeeklyActivity.java
+++ src/dataformats/WeeklyActivity.java
@@ -3,4 +3,46 @@ package dataformats;
 public class WeeklyActivity implements Activity
 {
 
+	@Override
+	public Time getStartTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Time getEndTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void markAsDone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStartTime(Time t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEndTime(Time t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLengthMinutes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
