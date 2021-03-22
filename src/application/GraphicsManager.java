ff29af868353e43d8d6c6eddd2e0d3fe59c2498d
Spencer Ye <sye348@student.fuhsd.org>
2021-03-21 21:29:12 -0700

    remove uneeded parts

--- src/application/GraphicsManager.java
+++ src/application/GraphicsManager.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
@@ -13,7 +12,6 @@ public class GraphicsManager
 		frame = new MainPanel("Personal Planner");
 		
 		frame.setSize(800, 600);
-		frame.setMinimumSize(new Dimension(100,100));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 
