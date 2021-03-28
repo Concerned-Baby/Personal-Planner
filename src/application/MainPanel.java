7db530b145dec2bf3ee5bcba2b8ea235cf27e6e6
Spencer Ye <sye348@student.fuhsd.org>
2021-03-28 16:09:44 -0700

    Update MainPanel.java

--- src/application/MainPanel.java
+++ src/application/MainPanel.java
@@ -6,21 +6,24 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MainPanel extends JFrame implements ActionListener
+public class MainPanel extends JFrame
 {
 
 	private static final long serialVersionUID = 1L;
+	
+	private ButtonManager buttonManager;
 
 	public MainPanel(String name) 
 	{
 		super(name);
+		buttonManager = new ButtonManager();
 	}
 	
 	public void setMainMenu()
 	{
 		JButton toCalander = new JButton("Go To Calander");
 		toCalander.setBounds(400, 400, 120, 80);
-		toCalander.addActionListener(this);
+		toCalander.addActionListener(buttonManager);
 	}
 	
 	public void setCalander()
@@ -28,9 +31,14 @@ public class MainPanel extends JFrame implements ActionListener
 		
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) 
+	private class ButtonManager implements ActionListener
 	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			//TODO
+		}
 		
 	}
 	
