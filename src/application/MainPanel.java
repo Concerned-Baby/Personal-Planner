2fac160eb4a6674329935a6ecad80c0d93cb1634
Don't Do Drugs <spencerye425@gmail.com>
2021-04-04 17:56:19 -0700

    Update MainPanel.java

--- src/application/MainPanel.java
+++ src/application/MainPanel.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class MainPanel extends JFrame
@@ -13,7 +14,7 @@ public class MainPanel extends JFrame
 	
 	private ButtonManager buttonManager;
 	
-	private JButton toCalander;
+	private JButton toCalender;
 
 	public MainPanel(String name) 
 	{
@@ -23,17 +24,24 @@ public class MainPanel extends JFrame
 	
 	public void setMainMenu()
 	{
-		toCalander = new JButton("Go To Calander");
-		toCalander.setBounds(400, 400, 120, 80);
-		toCalander.addActionListener(buttonManager);
+		toCalender = new JButton("Go To Calander");
+		toCalender.setBounds(400, 400, 120, 80);
+		toCalender.addActionListener(buttonManager);
+		this.add(toCalender);
 	}
 	
 	public void setCalander(int panel)
 	{
 		if (panel == 1) //from main menu
 		{
-			;;
+			this.remove(toCalender);
+			this.add(createCalender(0, 0));
 		}
+	}
+	
+	private JComponent createCalender(int month, int year)
+	{
+		return null;
 	}
 
 	private class ButtonManager implements ActionListener
@@ -42,7 +50,7 @@ public class MainPanel extends JFrame
 		@Override
 		public void actionPerformed(ActionEvent e) 
 		{
-			if (e.getSource().equals(toCalander))
+			if (e.getSource().equals(toCalender))
 			{
 				setCalander(1);
 			}
