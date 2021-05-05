b268e30d1597e3faf994cf0db19f5fae421db10c
Don't Do Drugs <spencerye425@gmail.com>
2021-05-04 20:45:12 -0700

    start menu

--- src/application/GraphicsManager.java
+++ src/application/GraphicsManager.java
@@ -13,6 +13,7 @@ public class GraphicsManager
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
+		frame.setVisible(true);
 		
 	}
 	
